81d4a7f17d55342909ed8d25f853dd3e5662b77e
Said Robley <saidrobley@gmail.com>
2018-03-17 21:52:13 -0600

    change

--- Youtube-Search-API/src/index.js
+++ Youtube-Search-API/src/index.js
@@ -9,22 +9,22 @@ const API_KEY= 'AIzaSyAi3AHKvr1vm5U4wEhpYh6TvXoEmmU0mZM';
 
 
 class App extends Component{
-  constructor(props) {
-    super(props);
-    this.state = {videos: []};
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos)=> {
-      this.setState({ videos });
-    });
-  }
-  render() {
-                  return (
-                    <div>
-                      <SearchBar />
-                      <VideoDetail video = {this.stat.videos[0]} />
+    constructor(props) {
+      super(props);
+      this.state = {videos: []};
+      YTSearch({key: API_KEY, term: 'surfboards'}, (videos)=> {
+        this.setState({ videos });
+      });
+    }
+    render() {
+                return (
+                  <div>
+                    <SearchBar />
+                      <VideoDetail video = {this.state.videos[0]} />
                       <VideoList videos = {this.state.videos} />
                     </div>
-                  )
-}
+                )
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'))
